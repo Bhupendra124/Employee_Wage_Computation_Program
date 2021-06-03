cc127aa20b37951c0952f59131a31e865672ab51
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-06-03 16:13:31 +0530

    Employee_PartTime_Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,26 @@
+#!/bin/bash -x
 
-                                       "Welcome to Employee Wage Computation Program"
+isFullTime=1
+isPartTime=2
+salaryPerHr=20
+empHr=0
+totalSalary=0
+
+randomNumber=$((RANDOM%3))
+
+if [ $randomNumber -eq $isFullTime ]
+ then
+	empHr=8
+	echo "Employee is FullTime"
+
+elif [ $randomNumber -eq $isPartTime ]
+ then
+	empHr=4
+	echo "Employee is part Time"
+else
+	empHr=0
+	echo "Employee is Absent"
+fi
+totalSalary=$(($salaryPerHr*$empHr))
+	echo "Salary of Employee is :-" $totalSalary
+
